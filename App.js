0bb99643a1cab8df26f985b915acb6179a4bac82
lukkasanches <lukadorock2008@gmail.com>
2026-08-24 16:35:40 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,39 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Movimento from './components/Movimento';
+import Pedometro from './components/Pedometro';
+import { Ionicons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Movimento" component={Movimento}
+                    options={{
+                      tabBarIcon: ({ color, size }) => (
+                        <Ionicons
+                          name="walk-outline"
+                          size={size}
+                          color={color}
+                        />
+                      )
+                    }}
+        />
+        <Tab.Screen name="Contador de Passos" component={Pedometro}
+                    options={{
+                      tabBarIcon: ({ color, size }) => (
+                        <Ionicons
+                          name="footsteps-outline"
+                          size={size}
+                          color={color}
+                        />
+                      )
+                    }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
